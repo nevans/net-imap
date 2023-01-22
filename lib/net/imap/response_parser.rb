4873a960875c90c677d288f53d869bdb19f00a7c
nicholas evans <nicholas.evans@gmail.com>
2023-01-22 17:29:31 -0500

    🚧 TODO: unhandled resp-text-code in UnparsedData
    Also, fallback (backtrack) parsing for any parse failures inside
    `resp-text-code`

--- lib/net/imap/response_parser.rb
+++ lib/net/imap/response_parser.rb
@@ -1823,14 +1823,17 @@ module Net
           when "MODIFIED"           then SP!; sequence_set         # CONDSTORE
           when "MAILBOXID"          then SP!; parens__objectid     # RFC8474: OBJECTID
           else
-            SP? and text_chars_except_rbra
+            SP? and resp_code__unhandled
           end
         ResponseCode.new(name, data)
       end
 
       alias resp_text_code__name case_insensitive__atom
 
-      # 1*<any TEXT-CHAR except "]">
+      # TODO: wrap string in UnparsedData
+      def resp_code__unhandled; text_chars_except_rbra end
+
+      #   [SP 1*<any TEXT-CHAR except "]">]
       def text_chars_except_rbra
         match_re(CTEXT_REGEXP, '1*<any TEXT-CHAR except "]">')[0]
       end
