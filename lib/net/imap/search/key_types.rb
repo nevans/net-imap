278a316001999ceda70093842aae7313dcb90fb6
nicholas evans <nicholas.evans@gmail.com>
2024-03-23 22:19:52 -0400

    ✨ all of the other KeyTypes [🚧 WIP]

--- lib/net/imap/search/key_types.rb
+++ lib/net/imap/search/key_types.rb
@@ -5,19 +5,59 @@ module Net
     class Search
 
       module KeyTypes
-        @mapping = {}
+        def self.search_key(const_name, types = {}, &)
+          key_type = new(const_name, types, &)
+          const_set const_name, key_type
+        end
 
-        def [](key_name) @mapping[key_name.downcase.to_sym] end
-
-        def self.search_key(const_name, type = nil, &)
+        def self.new(const_name, types = {}, &)
           key_name = const_name.to_sym.downcase
-          type = case type
-          when nil  then nullary(key_name.to_s, &)
-          when Hash then n_ary_search_key(key_name, type, &)
-          else           unary_search_key(key_name, type, &)
+          unless types.nil? || types.is_a?(Hash)
+            types = {key_name.to_sym.downcase => types}
           end
-          const_set const_name, type
-          @mapping[key_name] = type
+          if types
+            n_ary_search_key(key_name, types, &)
+          else
+            nullary(key_name, &)
+          end
+        end
+
+        def self.named(name)
+          key = Types::SearchKeyName[name].to_sym.downcase
+          Module.new do
+            def self.extended(mod) mod.const_set :Naming, self end
+            extend self
+
+            define_method(:key) { key }
+          end
+        end
+
+        def self.typed(types)
+          types.dup.freeze
+
+          Module.new do
+            const_set :TYPES, types
+
+            const_set(:ClassMethods, Module.new do
+              def member_types     = self::MemberTypes::TYPES
+              def member_type(key) = member_types.fetch(key)
+
+              def coerce(**kwargs)
+                kwargs.to_h {|key, value| [key, member_type(key)[value]] }
+              end
+            end)
+
+            singleton_class.define_method :included do |mod|
+              mod.const_set :MemberTypes, self
+              mod.extend self::ClassMethods
+            end
+
+            def initialize(**kwargs) = super(**coerce(**kwargs))
+
+            private
+
+            def coerce(...) = self.class.coerce(...)
+          end
         end
 
         class NullaryKey < Key
@@ -26,41 +66,20 @@ module Net
 
         def self.nullary(name, &block)
           NullaryKey.define do
-            extend KeyName[name]
+            extend KeyTypes.named name
           end
             .then { block ? Class.new(_1, &block) : _1 }
-        end
-
-        class UnaryKey < Key
-          def self.value = deconstruct.first
         end
 
         def self.unary_search_key(name, type, &block)
-          attr = name.to_sym.downcase
-          UnaryKey.define(attr) do
-            extend KeyName[name]
-            define_method :initialize do |**kwargs|
-              kwargs[attr] &&= type[kwargs[attr]]
-              super(**kwargs)
-            end
-          end
-            .then { block ? Class.new(_1, &block) : _1 }
+          n_ary_search_key(name, {name.to_sym.downcase => type}, &block)
         end
 
         def self.n_ary_search_key(name, types, &block)
           types.freeze
           Key.define(*types.keys) do
-            extend KeyName[name]
-
-            singleton_class.define_method(:member_types) { types }
-
-            def initialize(**kwargs)
-              self.class.member_types.each do |attr, type|
-                next unless type
-                kwargs[attr] &&= type[kwargs[attr]]
-              end
-              super(**kwargs)
-            end
+            extend  KeyTypes.named name
+            include KeyTypes.typed types if types
           end
             .then { block ? Class.new(_1, &block) : _1 }
         end
@@ -79,33 +98,114 @@ module Net
         search_key :Seen
         search_key :Unseen
 
-        search_key :Keyword,    Types::FlagKeyword
-        search_key :Unkeyword,  Types::FlagKeyword
+        search_key :Keyword,      Types::FlagKeyword
+        search_key :Unkeyword,    Types::FlagKeyword
 
         search_key :Seq, SequenceSet do
           def name = key
           def to_a = [seq]
         end
 
-        search_key :UID,        SequenceSet
+        search_key :UID,          SequenceSet
 
-        search_key :Filter,     Types::FilterName
+        search_key :Filter,       Types::FilterName
 
-        search_key :EmailID,    Types::ObjectID
-        search_key :ThreadID,   Types::ObjectID
+        search_key :EmailID,      Types::ObjectID
+        search_key :ThreadID,     Types::ObjectID
 
-        search_key :From,       Types::EnvelopeField
-        search_key :To,         Types::EnvelopeField
-        search_key :Cc,         Types::EnvelopeField
-        search_key :Bcc,        Types::EnvelopeField
-        search_key :Subject,    Types::EnvelopeField
+        # Substring matching (should be case-insensitive)
+        search_key :From,         Types::EnvelopeField
+        search_key :To,           Types::EnvelopeField
+        search_key :Cc,           Types::EnvelopeField
+        search_key :Bcc,          Types::EnvelopeField
+        search_key :Subject,      Types::EnvelopeField
 
-        search_key :Body,       Types::FullText
-        search_key :Text,       Types::FullText
+        # "Full text" searching (may use stemming, etc)
+        search_key :Body,         Types::FullText
+        search_key :Text,         Types::FullText
 
-        search_key :Header,
-          field_name: Types::HeaderFldName,
-          string:     Types::Astring
+        # Internal Date
+        search_key :Before,       Types::Date
+        search_key :On,           Types::Date
+        search_key :Since,        Types::Date
+
+        # "Date:" header
+        search_key :SentBefore,   Types::Date
+        search_key :SentOn,       Types::Date
+        search_key :SentSince,    Types::Date
+
+        # SAVEDATE extension [RFC8514]
+        search_key :SavedBefore,  Types::Date
+        search_key :SavedOn,      Types::Date
+        search_key :SavedSince,   Types::Date
+
+        # RFC822.SIZE
+        search_key :Larger,       Types::Number64
+        search_key :Smaller,      Types::Number64
+
+        # Internal Date (WITHIN extension [RFC5032])
+        search_key :Older,        Types::NzNumber
+        search_key :Younger,      Types::NzNumber
+
+        search_key(:Header,
+                   field_name: Types::HeaderFldName,
+                   substring:  Types::Astring)
+
+        # Modification sequence number (CONDSTORE extension [RFC7162])
+        search_key(:ModSeq,
+                   entry_name:     Types::EntryName,
+                   entry_type_req: Types::EntryTypeReq,
+                   modseq:         Types::ModSequenceValzer) do
+
+          def self.[](*args, **kwargs)
+            (args in [modseq]) ? super(nil, nil, modseq, **kwargs) : super
+          end
+
+          def initialize(...)
+            super
+            entry_name && !entry_type_req and
+              raise DataFormatError, "missing entry-type-req"
+            !entry_name && entry_type_req and
+              raise DataFormatError, "missing entry-name"
+          end
+
+          def deconstruct = super.compact
+        end
+
+        # Annotations (ANNOTATE-EXPERIMENT-1 extension [RFC5257])
+        search_key(:Annotation,
+                   entry_match: Types::EntryMatch,
+                   att:         Types::AttSearch,
+                   data:        Types::NString8)
+
+        search_key(:Generic,
+                   name: Types::SearchKeyName,
+                   args: method(:Array)) do
+
+          def self.[](*args, **kwargs)
+            (args in name, *rest) ? super(name, rest, **kwargs) : super
+          end
+
+          def deconstruct = to_a
+        end
+
+        # See https://developers.google.com/gmail/imap/imap-extensions#extension_of_the_search_command_x-gm-raw
+        search_key :XGmRaw, Types::Astring do
+          def name = "X-GM-RAW"
+          def key  = :x_gm_raw
+        end
+
+        # See https://developers.google.com/gmail/imap/imap-extensions#access_to_the_unique_message_id_x-gm-msgid
+        search_key :XGmMsgID, Types::UInt64 do
+          def name = "X-GM-MSGID"
+          def key  = :x_gm_msgid
+        end
+
+        # See https://developers.google.com/gmail/imap/imap-extensions#access_to_the_thread_id_x-gm-thrid
+        search_key :XGmThrID, Types::UInt64 do
+          def name = "X-GM-THRID"
+          def key  = :x_gm_thrid
+        end
 
       end
 
--- lib/net/imap/types.rb
+++ lib/net/imap/types.rb
@@ -9,12 +9,15 @@ module Net
     module Types # :nodoc: all
 
       module Formats
-        atom_char   = ResponseParser::Patterns::ATOM_CHAR
-        ASTRING     = /\A[^\0]*\z/n
-        ATOM        = /\A#{ResponseParser::Patterns::ATOM}\z/n
-        FILTER_NAME = %r{\A[#{atom_char.source}&&[^/]]+\z}n
-        LABEL       = /\A#{ResponseParser::Patterns::TAGGED_EXT_LABEL}\z/n
-        OBJECTID    = /\A#{ResponseParser::Patterns::OBJECTID}\z/n
+        atom_char      = ResponseParser::Patterns::ATOM_CHAR
+        ASTRING        = /\A[^\0]*\z/n
+        ATOM           = /\A#{ResponseParser::Patterns::ATOM}\z/n
+        FILTER_NAME    = %r{\A[#{atom_char.source}&&[^/]]+\z}n
+        LABEL          = /\A#{ResponseParser::Patterns::TAGGED_EXT_LABEL}\z/n
+        OBJECTID       = /\A#{ResponseParser::Patterns::OBJECTID}\z/n
+
+        ENTRY_TYPE_REQ = /\A(?:all|priv|shared)\z/i
+        ATT_SEARCH     = /\Avalue(?:\.(?:priv|shared))?\z/i
       end
 
       LabelType = ->(name, regexp = Formats::LABEL) {
@@ -22,7 +25,7 @@ module Net
         ->(value) { validation[value.is_a?(Symbol) ? value.to_s : value] }
       }
 
-      StringType = ->(name, regexp) {
+      StringType = ->(name, regexp = Formats::ASTRING) {
         ->(value) {
           value = String.try_convert(value) or
             raise TypeError, "expected String, got %s" % [value.class]
@@ -43,6 +46,9 @@ module Net
         }
       }
 
+      OptionalType = ->(type) { ->(value) { value.nil? ? nil : type[value] } }
+      NStringType = ->(*args) { OptionalType[StringType[*args]] }
+
       Date = ->(value) {
         if value.respond_to?(:to_date)
           value = value.to_date
@@ -59,22 +65,33 @@ module Net
       # use the tagged-ext-label format, using another format seems unlikely.
       SearchKeyName  = LabelType["search-key name"]
 
-      FlagKeyword   = StringType["flag-keyword",    Formats::ATOM]
+      NString8      = NStringType["nstring or literal8", //]
+      NString       = StringType["nstring",         Formats::ASTRING]
       Astring       = StringType["astring",         Formats::ASTRING]
+      FlagKeyword   = StringType["flag-keyword",    Formats::ATOM]
+      ListMailbox   = StringType["list-mailbox",    Formats::ASTRING]
       HeaderFldName = StringType["header-fld-name", Formats::ASTRING]
       FilterName    = StringType["filter-name",     Formats::FILTER_NAME]
       ObjectID      = StringType["objectid",        Formats::OBJECTID]
+
       EnvelopeField = Astring
       FullText      = Astring
 
       Number            = NumberType["number",              min: 0, bits: 32]
       NzNumber          = NumberType["nz-number",           min: 1, bits: 32]
+      Number64          = NumberType["number64",            min: 0, bits: 63]
+      NzNumber64        = NumberType["nz-number64",         min: 1, bits: 63]
       ModSequenceValue  = NumberType["mod-sequence-value",  min: 1, bits: 63]
       ModSequenceValzer = NumberType["mod-sequence-valzer", min: 0, bits: 63]
+      UInt64            = NumberType["uint64",              min: 0, bits: 64]
 
-      EntryName    = StringType["search-modseq-ext entry-name", Formats::ASTRING]
-      EntryTypeReq = StringType["search-modseq-ext entry-type-req",
-                                /\A(?:all|priv|shared)\z/i]
+      EntryName    = NStringType["search-modseq-ext entry-name",
+                                 Formats::ASTRING]
+      EntryTypeReq = NStringType["search-modseq-ext entry-type-req",
+                                 Formats::ENTRY_TYPE_REQ]
+
+      EntryMatch   = StringType["entry-match", Formats::ASTRING]
+      AttSearch    = StringType["att-search", Formats::ATT_SEARCH]
 
     end
   end
